00841a0be8458988355a9ad1270c65f5400228e3
interpretivechaos <ryan_maloney@brown.edu>
2016-01-19 10:38:48 -0500

    First Commit

---
+++ luminance.py
@@ -0,0 +1,7 @@
+#luminance.py
+
+import csv
+with open('AppleLum.csv', newline='') as csvfile:
+     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+     for row in spamreader:
+         print(', '.join(row))
